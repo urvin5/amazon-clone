ab9e4f00cb28f536f47dd3b36014877beb118172
Urvin shah <urvinshah5@gmail.com>
2021-02-15 20:27:31 +0530

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../Images/amazon-2.svg";
+import "../Styles/header.css";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import SearchIcon from "@material-ui/icons/Search";
+import { IconButton, InputBase } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+function Header() {
+  return <div className="header"></div>;
+}
+
+export default Header;
---
+++ src/Components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className="home">Hello World Again!!</div>;
+}
+
+export default Home;
